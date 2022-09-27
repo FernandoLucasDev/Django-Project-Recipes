e4aca2ed751bf3495d93405c04f756e5206001e7
Fernando Lucas <nandopm2004@gmail.com>
2022-09-27 12:55:11 -0300

    new upgrades

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'nome': 'fernando'})
-
-
-def sobre(request):
-    return HttpResponse('SOBRE 1')
-
-
-def contato(request):
-    return HttpResponse('CONTATO 1')
+    return render(request, 'recipes/pages/home.html', context={'nome': 'fernando'})
