09881a36458da96a72c00663bd35c21690b24271
Fernando Lucas <nandopm2004@gmail.com>
2022-11-08 14:39:36 -0300

    finish of section 17

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,8 @@ import os
 
 from django.core.paginator import Paginator
 from django.db.models import Q
+from django.forms.models import model_to_dict
+from django.http import JsonResponse
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.views.generic import DetailView, ListView
@@ -43,6 +45,19 @@ class RecipeListViewHome(RecipeListViewBase):
     template_name = 'recipes/pages/home.html'
 
 
+class RecipeListViewHomeApi(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
+
+    def render_to_response(self, context, **response_kwargs):
+        recipes = self.get_context_data()['recipes']
+        recipes_list = recipes.object_list.values()
+        # print(recipes.object_list)
+        return JsonResponse(
+            list(recipes_list),
+            safe=False
+        )
+
+
 class RecipeListViewCategory(RecipeListViewBase):
     template_name = 'recipes/pages/category.html'
 
@@ -82,6 +97,28 @@ class RecipeDetail(DetailView):
         })
 
         return ctx
+
+
+class RecipeDetailApi(RecipeDetail):
+    def render_to_response(self, context, **response_kwargs):
+        recipe = self.get_context_data()['recipes']
+        recipe_dict = model_to_dict(recipe)
+
+        recipe_dict['created_at'] = str(recipe.created_at)
+        recipe_dict['updated_at'] = str(recipe.updated_at)
+
+        if recipe_dict.get('cover'):
+            recipe_dict['cover'] = self.request.build_absolute_uri() + \
+                recipe_dict['cover'].url[1:]
+        else:
+            recipe_dict['cover'] = ''
+
+        del recipe_dict['is_published']
+
+        return JsonResponse(
+            recipe_dict,
+            safe=False,
+        )
 
 
 def home(request):
